e8226136948f1282cfa1a29a336374796550eabd
RAVITEJ SANDHU <144312605+OGsandhu@users.noreply.github.com>
2025-07-21 13:31:48 +0530

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,88 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
+from django.http import HttpResponse
+import csv
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'phone', 'is_active', 'date_joined', 'last_login')
+    list_filter = ('role', 'is_active', 'is_staff', 'is_superuser', 'date_joined')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'phone')
+    ordering = ('-date_joined',)
+    
+    # Fieldsets for the add form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role', 'phone'),
+        }),
+    )
+    
+    # Fieldsets for the change form
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'role')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    readonly_fields = ('date_joined', 'last_login')
+    
+    def get_queryset(self, request):
+        """Show all users to admins, but filter for non-superusers"""
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(is_superuser=False)
+    
+    def has_delete_permission(self, request, obj=None):
+        """Prevent deletion of superusers by non-superusers"""
+        if obj and obj.is_superuser and not request.user.is_superuser:
+            return False
+        return super().has_delete_permission(request, obj)
+    
+    def has_change_permission(self, request, obj=None):
+        """Prevent modification of superusers by non-superusers"""
+        if obj and obj.is_superuser and not request.user.is_superuser:
+            return False
+        return super().has_change_permission(request, obj)
+    
+    actions = ['activate_users', 'deactivate_users', 'export_user_data']
+    
+    def activate_users(self, request, queryset):
+        """Activate selected users"""
+        updated = queryset.update(is_active=True)
+        self.message_user(request, f'{updated} users have been activated.')
+    activate_users.short_description = "Activate selected users"
+    
+    def deactivate_users(self, request, queryset):
+        """Deactivate selected users"""
+        updated = queryset.update(is_active=False)
+        self.message_user(request, f'{updated} users have been deactivated.')
+    deactivate_users.short_description = "Deactivate selected users"
+    
+    def export_user_data(self, request, queryset):
+        """Export user data to CSV"""
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="users_export.csv"'
+        
+        writer = csv.writer(response)
+        writer.writerow(['Username', 'Email', 'First Name', 'Last Name', 'Role', 'Phone', 'Date Joined'])
+        
+        for user in queryset:
+            writer.writerow([
+                user.username,
+                user.email,
+                user.first_name,
+                user.last_name,
+                user.role,
+                user.phone,
+                user.date_joined.strftime('%Y-%m-%d %H:%M:%S')
+            ])
+        
+        return response
+    export_user_data.short_description = "Export selected users to CSV"
